bce05cba402452bcee712acab9ec488c4fa9ae44
Keshav Agarwal <keshav@multivariate.tech>
2022-08-23 11:13:44 +0530

    add xml support

--- extractor.py
+++ extractor.py
@@ -37,8 +37,9 @@ extlist = set()
 intlist = set()
 
 try:
-    for link in to_scrape:
-        print("Left to scrape : ", len(to_scrape), '\nScraped : ', len(scraped), '\n')
+    while to_scrape != []:
+        link = to_scrape[0]
+        print("Links : ", len(to_scrape), '\nScraped : ', len(scraped), '\n')
         try:
         
             headers = {'Connection': 'keep-alive',
@@ -51,47 +52,70 @@ try:
                 'referer': random.choice(reffer_list)}
 
             page = requests.get(link, headers=headers)
-            soup = BeautifulSoup(page.text, "lxml")
 
-
-            for a in soup.findAll("a", attrs={"href": True}):
-                if (
-                    len(a["href"].strip()) > 1
-                    and a["href"][0] != "#"
-                    and "javascript:" not in a["href"].strip()
-                    and "mailto:" not in a["href"].strip()
-                    and "tel:" not in a["href"].strip()
-                ):
-                    if "http" in a["href"].strip() or "https" in a["href"].strip():
+            if link.endswith('.xml') or link.endswith('.xml/'):
+                soup = BeautifulSoup(page.text, "xml")
+                for a in soup.findAll("loc"):
+                    l = a.get_text()
+                    if "http" in l.strip() or "https" in l.strip():
                         if (
                             urlparse(link).netloc.lower()
-                            in urlparse(a["href"].strip()).netloc.lower()
+                            in urlparse(l.strip()).netloc.lower()
                         ):
+                            intlist.add(l)
+                            if l not in scraped and l not in to_scrape:
+                                to_scrape.append(l)
+                        else:
+                            extlist.add(l)
+                    else:
+                        intlist.add(l)
+                        if l not in scraped and l not in to_scrape:
+                            to_scrape.append(l)
+            
+            else:
+                soup = BeautifulSoup(page.text, "lxml")
+                for a in soup.findAll("a", attrs={"href": True}):
+                    if (
+                        len(a["href"].strip()) > 1
+                        and a["href"][0] != "#"
+                        and "javascript:" not in a["href"].strip()
+                        and "mailto:" not in a["href"].strip()
+                        and "tel:" not in a["href"].strip()
+                    ):
+                        if "http" in a["href"].strip() or "https" in a["href"].strip():
+                            if (
+                                urlparse(link).netloc.lower()
+                                in urlparse(a["href"].strip()).netloc.lower()
+                            ):
+                                intlist.add(a["href"])
+                                if a["href"] not in scraped and a["href"] not in to_scrape:
+                                    to_scrape.append(a["href"])
+                            else:
+                                extlist.add(a["href"])
+                        else:
                             intlist.add(a["href"])
                             if a["href"] not in scraped and a["href"] not in to_scrape:
                                 to_scrape.append(a["href"])
-                        else:
-                            extlist.add(a["href"])
-                    else:
-                        intlist.add(a["href"])
-                        if a["href"] not in scraped and a["href"] not in to_scrape:
-                            to_scrape.append(a["href"])
+
+
         except:
             pass
-        to_scrape.remove(link)
+        del to_scrape[0]
         scraped.add(link)
 
     print("---------------------")
     print("\n")
     print(str(len(intlist)) + " internal links found:")
     print("\n")
-    for il in intlist:
-        print(il)
+    with open('interalinks.txt', 'w') as int_f:
+        for il in intlist:
+            int_f.write(str(il)+'\n')
     print("\n")
     print(str(len(extlist)) + " external links found:")
     print("\n")
-    for el in extlist:
-        print(el)
+    with open('externalinks.txt', 'w') as ext_f:
+        for el in extlist:
+            ext_f.write(str(el)+'\n')
     print("\n")
 
 except Exception as e:
